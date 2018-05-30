19cb0acd479f307a61627ec56780fad8501c96e1
Mateo Senoner <csap3466@uibk.ac.at>
2018-05-30 16:09:30 +0200

    Grundkarten hinzugefügt

--- mountainbike/mtbike.js
+++ mountainbike/mtbike.js
@@ -9,17 +9,31 @@ let myLayers = {
         	subdomains : ["a","b","c"],
 			attribution : "© <a href= 'https://www.openstreetmap.org/copyright'>OpenStreetMap</a> Mitwirkende"
         }
+    ),
+    geolandbasemap : L.tileLayer(
+        "https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png", {
+            subdomains : ["maps","maps1","maps2","maps3","maps4"], 
+            attribution : "Datenquelle: <a href= 'https://www.basemap.at'>Basemap.at</a>" 
+        }
+	),
+	bmaporthofoto30cm: L.tileLayer(
+        "https://{s}.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg", {
+            subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
+            attribution: "Datenquelle: <a href='https://www.basemap.at'>basemap.at</a>"
+        }
     )
 };
 
 myMap.addLayer(myLayers.osm); 
 
 let myMapControl = L.control.layers({ 
-    "Openstreetmap" : myLayers.osm
+	"Openstreetmap" : myLayers.osm,
+	"basemap.at Grundkarte": myLayers.geolandbasemap,
+    "basemap.at Orthofoto": myLayers.bmaporthofoto30cm,
 });
 
 myMap.addControl(myMapControl); 
-myMap.setView([46.499,11.3566], 10); 
+myMap.setView([47.8,13.033333], 9); 
 
 L.control.scale({ 
 	position: "bottomleft", 
@@ -27,3 +41,13 @@ L.control.scale({
 	metric: true, 
 	imperial: false, 
 }).addTo(myMap); 
+
+const url = "http://www.salzburg.gv.at/ogd/e5bc00bf-a84a-46fa-9494-b43ba606f1f6/Mountainbike_Strecken.json"
+
+async function addGeojson(url) {
+	console.log("Url wird geladen: ", url);
+	const response = await fetch(response);
+	console.log("Response: ", response);
+};
+
+
